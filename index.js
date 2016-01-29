a1beef21b3dc11d4539ec69c20a75b58070f4e83
Douglas Conrad <conrad@opens.com.br>
2016-01-29 16:44:41 -0200

    - modified call control flow to prevent more than 2 channels in the same call - implemented register of call flow control to register into bill all calls informations in the bridge - sending to the webhook a new format of billing like this: 	{"id":1,"callid":"1454092584.625","calldate":"2016-01-29T18:36:24.984Z","from":"1008","fromname":"Ramal 1008","to":"1009","route":"SIP/1009","dstchannel":"SIP/1009-00000129","channel":"SIP/1008-00000128","status":"6","linkedid":"1454092605.629","toname":"<unknown>", 		"callflow": 		[ 			{ 			"id":2,"callid":"1454092586.626","from":"1009","to":"s","channel":"SIP/1009-00000129","status":"6","calldate":"2016-01-29T18:36:26.265Z","answerdate":"2016-01-29T18:36:32.384Z","duration":"30","billsec":"24" 			}, 			{ 			"id":3,"callid":"1454092605.627","from":"<unknown>","to":"1005","channel":"Local/1005@transferencias-000000a2;1","status":"6","calldate":"2016-01-29T18:36:45.327Z","answerdate":"2016-01-29T18:36:47.704Z","duration":"7","billsec":"5" 			}, 			{ 			"id":4,"callid":"1454092605.628","from":"<unknown>","to":"1005","channel":"Local/1005@transferencias-000000a2;2","status":"6","calldate":"2016-01-29T18:36:45.327Z","answerdate":"2016-01-29T18:36:47.703Z","duration":"7","billsec":"5" 			}, 			{ 			"id":5,"callid":"1454092605.629","from":"1005","to":"s","channel":"SIP/1005-0000012a","status":"6","calldate":"2016-01-29T18:36:45.917Z","answerdate":"2016-01-29T18:36:47.696Z","duration":"11","billsec":"9" 			} 		], 	"answerdate":"2016-01-29T18:36:32.384Z","hangupdate":"2016-01-29T18:36:56.547Z","duration":"32","billsec":"19" 	}

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({
+	name: "asterisk-billing",
+	streams:
+    [
+    {
+      stream: process.stdout,
+      level: 'debug'
+    },
+    {
+      path: 'billing.log',
+      level: 'info'
+    }
+    ]
+});
+
 var ami = new require('asterisk-manager')('5038','localhost', 'snep', 'sneppass', true);
 
 var mysql = require('mysql');
@@ -12,7 +28,7 @@ var util = require('util');
 // In case of any connectiviy problems we got you coverd.
 ami.keepConnected();
 
-console.log("Starting the observer for now calls");
+log.info("Starting the observer for now calls");
 
 
 var newchannel = Rx.Observable.fromEvent(ami,'newchannel');
@@ -26,19 +42,37 @@ var joining = Rx.Observable.merge(
 	hangup);
 
 var bill = [];
+var count = [];
 
 var billing = joining.subscribe(
 	function(x){
 		var date = new Date();
 		if(x.event === 'Newchannel'){
-			if(x.linkedid !== x.uniqueid){
+			//var uniqueid = x.uniqueid;
+			log.info('DEBUG: Newchannel -> %s', JSON.stringify(x));
+			if(bill[x.linkedid]){
+				if(bill[x.linkedid].route === 'local'){
 					bill[x.linkedid].route = x.channel.split('-')[0];
-					bill[x.linkedid].dstchannel = x.channel;
-					bill[x.linkedid].linkedid = x.uniqueid;
-					console.log("New channel in a already monitored call: %s", JSON.stringify(bill[x.linkedid]));
-			}else{
+					bill[x.linkedid].dstchannel = x.channel;	
+				}
+				bill[x.linkedid].linkedid = x.uniqueid;
+				bill[x.linkedid].callflow.push({
+					id: ++count[x.linkedid].count,
+					callid: x.uniqueid,
+					from: x.calleridnum,
+					to: x.exten,
+					channel: x.channel,
+					status: x.channelstate,
+					calldate: date
+				});
+				log.info("New channel in a already monitored call: %s", JSON.stringify(bill[x.linkedid]));
 				
+			}else if(!bill[x.uniqueid]){
+				count[x.uniqueid] = {
+					count:1
+				};
 				bill[x.uniqueid] = {
+					id: count[x.uniqueid].count,
 					callid: x.uniqueid,
 					calldate: date,
 					from: x.calleridnum,
@@ -49,38 +83,91 @@ var billing = joining.subscribe(
 					channel: x.channel,
 					status: x.channelstate,
 					linkedid: x.linkedid,
-					toname: "<unknown>"
+					toname: "<unknown>",
+					callflow: []
 				};
-				console.log("New call monitored: %s", JSON.stringify(bill[x.uniqueid]));
+				log.info("New call monitored: %s", JSON.stringify(bill[x.uniqueid]));
+			}else{
+				log.info("DEBUG: Unmonitored Channel -> %s", JSON.stringify(x));
 			}
+			
 		}
 		if(x.event === 'Newstate'){
 			try {
-				if(x.channelstate === '6' && (!bill[x.uniqueid].answerdate)){
+				if(bill[x.uniqueid]){
 					bill[x.uniqueid].answerdate = date;
 					bill[x.uniqueid].status = x.channelstate;
-					console.log('Call was ANSWERED: %s', JSON.stringify(x));
+					if(x.channelstate === '6'){
+						log.info('Call was ANSWERED by the caller: %s', JSON.stringify(bill[x.uniqueid]));
+					}
+				}else if(bill[x.linkedid]){
+					for (var i = bill[x.linkedid].callflow.length - 1; i >= 0; i--) {
+						if(bill[x.linkedid].callflow[i].callid === x.uniqueid){
+							bill[x.linkedid].callflow[i].status = x.channelstate;
+							if(x.channelstate === '6'){
+								bill[x.linkedid].callflow[i].answerdate = date;
+								log.info('Call was ANSWERED by the called: %s', JSON.stringify(bill[x.linkedid]));
+							}
+						}
+					}
 				}
 			}catch (err){
-				//console.log("I don't know what is happing here! %s", err);
-				//console.log("Event: %s", JSON.stringify(x));
+				log.info(err,"Error in a Newstate: %s", err);
+				log.info(err, "DEBUG: Newstate Error -> %s", JSON.stringify(x));
 			}
 		}
 		if(x.event === 'Hangup'){
-			//console.log("Hanguping a channel: %s", JSON.stringify(x));
-			try {
-				bill[x.uniqueid].hangupdate = date;
-				bill[x.uniqueid].status = x.channelstate;
-				bill[x.uniqueid].duration = ((bill[x.uniqueid].hangupdate - bill[x.uniqueid].calldate) / 1000).toFixed(0);
-				if(bill[x.uniqueid].answerdate){
-					bill[x.uniqueid].billsec = ((bill[x.uniqueid].hangupdate - bill[x.uniqueid].answerdate) / 1000).toFixed(0);	
+			//try {
+				if(bill[x.uniqueid]){
+					count[x.uniqueid].count = count[x.uniqueid].count - 1;
+					bill[x.uniqueid].status = x.channelstate;
+					bill[x.uniqueid].hangupdate = date;
+					bill[x.uniqueid].duration = (
+						(bill[x.uniqueid].hangupdate - bill[x.uniqueid].calldate) / 1000)
+						.toFixed(0);
+					if(bill[x.uniqueid].answerdate){
+						bill[x.uniqueid].billsec = ((bill[x.uniqueid].hangupdate - bill[x.uniqueid].answerdate) / 1000).toFixed(0);	
+					}else{
+						bill[x.uniqueid].billsec = '0';
+					}
+					log.info('DEBUG: call [%s] Hanguping the caller channel on the bridge: %s',count[x.uniqueid].count, JSON.stringify(bill[x.uniqueid]));
+					if(count[x.uniqueid].count === 0){
+						savebill(bill[x.uniqueid]);	
+					}
+				}else if(bill[x.linkedid]){
+					//bill[x.linkedid].status = x.channelstate;
+					count[x.linkedid].count = count[x.linkedid].count - 1;
+					bill[x.linkedid].hangupdate = date;
+					bill[x.linkedid].duration = (
+						(bill[x.linkedid].hangupdate - bill[x.linkedid].calldate) / 1000)
+						.toFixed(0);
+					for (var i = bill[x.linkedid].callflow.length - 1; i >= 0; i--) {
+						if(bill[x.linkedid].callflow[i].callid === x.uniqueid){
+							bill[x.linkedid].callflow[i].duration = (
+								(bill[x.linkedid].hangupdate - bill[x.linkedid].callflow[i].calldate) / 1000)
+								.toFixed(0);
+							if(bill[x.linkedid].callflow[i].answerdate){
+								bill[x.linkedid].callflow[i].billsec = (
+									(bill[x.linkedid].hangupdate - bill[x.linkedid].callflow[i].answerdate) / 1000)
+								.toFixed(0);	
+							}else{
+								bill[x.linkedid].callflow[i].billsec = '0';
+							}
+						}
+					}
+					log.info('DEBUG: call [%s] Hanguping the called channel on the bridge: %s',count[x.linkedid].count, JSON.stringify(bill[x.linkedid]));
+					if(count[x.linkedid].count === 0){
+						savebill(bill[x.linkedid]);	
+					}
 				}else{
-					bill[x.uniqueid].billsec = '0';
+					log.info("DEBUG: Hanguping an unmonitoring channel: %s", JSON.stringify(x));	
 				}
-				savebill(bill[x.uniqueid]);	
-			}catch (err){
-				//console.log("Hanguping an unmonitoring channel: %s", JSON.stringify(x));
-			}
+				
+				
+			//}catch (err){
+			//	log.info('Error: %s', err);
+			//	log.info("Hanguping an unmonitoring channel: %s", JSON.stringify(x));
+			//}
 			
 		}
 		
@@ -96,8 +183,9 @@ function savebill(bill){
 	if(bill.hangupdate){
 		if(!bill.answerdate){ bill.answerdate = ''; };
 		var savecdr = abilling.savecdr(bill);
-		console.log("Closing billing:" + JSON.stringify(bill));	
-		console.log("<=======================================================>");
+		log.info("Closing billing:" + JSON.stringify(bill));	
+		log.info("<=======================================================>");
+		return;
 	}
 }
 
